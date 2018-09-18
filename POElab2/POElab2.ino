48a58a4c6da87eaaee53ea170871e7c8ede13d0d
hadleigh-000 <31555834+hadleigh-000@users.noreply.github.com>
2018-09-18 08:40:36 -0400

    adding basic and untested servo sweep to arduino sketch

--- POElab2/POElab2.ino
+++ POElab2/POElab2.ino
@@ -1,18 +1,44 @@
 
+
+#include <Servo.h>
+Servo servoH;
+Servo servoV; 
+ 
 const int analogInPin = A0;  // Analog input pin that the potentiometer is attached to
 int sensorValue = 0; 
+int posH = 60;
+int posV = 70; // we may want to change this, it might be too angled
+
 
 void setup() {
   // put your setup code here, to run once:
   long baudRate = 9600;       // NOTE1: The baudRate for sending & receiving programs must match
   Serial.begin(baudRate);     //
+  servoH.attach(9);
+  servoV.attach(10);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
   sensorValue = analogRead(analogInPin);
   Serial.println(sensorValue);
-  
   delay(50);
+  for (posV = 70; posv <= 110; posV += 1) {
+    if (posH <= 60) { 
+      for (posH = 60; posH <= 120; posH +=1) {
+        servoH.write(posH);
+        delay(20);
+      }
+    }
+    else {
+      for (posH = 120; posH >= 60; posH -=1) {
+        servoH.write(posH); 
+        delay(20);
+      }
+    }
+    servoV.write(posV); 
+    delay(50)
+    }
+  
 
 }
